0e9607b7fa31521e31e9dcbab9d3ec3773358742
Sebastian Germer <sebastian.germer@dfki.de>
2025-06-24 10:00:27 +0200

    added new registries

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ starttime=$(date)
 for months in "6" "12" "18" "24"
 do
     echo "Running for $months months"
-    for i in "all" "1" "2" "3" "5" "10" "14" "15"
+    for i in "all" "1" "2" "3" "4" "5" "6" "7" "9" "10" "13" "14" "15" "16"
     do
         echo "Running for registry $i"
         python main.py --registry=$i --months=$months --inverse --dummy --data_path=$data_path --entity=$entity
